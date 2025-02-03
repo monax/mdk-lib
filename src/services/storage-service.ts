8257d8eb9138892ad5d7d188209c47ffdfba85dc
Daniel Zahariev <daniel.zahariev@monaxlabs.io>
2025-02-03 11:55:05 +0200

    Upgrade Storage service

--- src/services/storage-service.ts
+++ src/services/storage-service.ts
@@ -1,5 +1,5 @@
 import fs from 'node:fs';
-import { Storage } from '@google-cloud/storage';
+import { type GetAclResponse, Storage } from '@google-cloud/storage';
 import type { SaveData, SaveOptions } from '@google-cloud/storage/build/esm/src/file.js';
 import { type StorageConfig, getKnownExtensionMimeType } from '@monaxlabs/mdk-schema';
 import { z } from 'zod';
@@ -11,7 +11,7 @@ export const BucketType = z.enum(['public', 'private']);
 
 export type IStorageService = InstanceType<typeof StorageService>;
 
-export class StorageService implements IStorageService {
+export class StorageService<B extends BucketType> implements IStorageService {
   readonly storage: Storage;
 
   constructor(protected readonly config: StorageConfig) {
@@ -21,15 +21,15 @@ export class StorageService implements IStorageService {
     });
   }
 
-  protected bucketName(type: BucketType) {
+  protected bucketName(type: B) {
     return type === 'public' ? this.config.GCS_PUBLIC_FILES_BUCKET : this.config.GCS_PRIVATE_FILES_BUCKET;
   }
 
-  cdnUrl(type: BucketType, remotePath: string): string {
+  cdnUrl(type: B, remotePath: string): string {
     return `${this.config.GCS_HOST}/${this.bucketName(type)}/${remotePath}`;
   }
 
-  async saveFile(type: BucketType, remotePath: string, data: SaveData, options?: SaveOptions): Promise<string> {
+  async saveFile(type: B, remotePath: string, data: SaveData, options?: SaveOptions): Promise<string> {
     await this.storage.bucket(this.bucketName(type)).file(remotePath).delete({ ignoreNotFound: true });
 
     await this.storage.bucket(this.bucketName(type)).file(remotePath).save(data, options);
@@ -37,7 +37,7 @@ export class StorageService implements IStorageService {
     return this.cdnUrl(type, remotePath);
   }
 
-  async deleteFile(type: BucketType, pathOrUrl: string): Promise<void> {
+  async deleteFile(type: B, pathOrUrl: string): Promise<void> {
     const remotePath = pathOrUrl.replace(`${this.config.GCS_HOST}/${this.bucketName(type)}/`, '').replace(/\?.*$/, '');
 
     const remoteFile = this.storage.bucket(this.bucketName(type)).file(remotePath);
@@ -46,7 +46,7 @@ export class StorageService implements IStorageService {
     }
   }
 
-  async getViewUrl(type: BucketType, remotePath: string): Promise<string> {
+  async getViewUrl(type: B, remotePath: string): Promise<string> {
     const [url] = await this.storage
       .bucket(this.bucketName(type))
       .file(remotePath)
@@ -63,7 +63,7 @@ export class StorageService implements IStorageService {
    * @link https://cloud.google.com/storage/docs/samples/storage-generate-upload-signed-url-v4#storage_generate_upload_signed_url_v4-nodejs
    */
   async getUploadUrl(
-    type: BucketType,
+    type: B,
     remotePath: string,
     { contentType }: { contentType?: string } = {},
   ): Promise<{ uploadUrl: string; remotePath: string; publicUrl: string; contentType: string }> {
@@ -81,5 +81,20 @@ export class StorageService implements IStorageService {
 
     const publicUrl = type === 'public' ? this.cdnUrl(type, remotePath) : await this.getViewUrl(type, remotePath);
     return { uploadUrl: url, remotePath, publicUrl, contentType };
+  }
+
+  async getMeta(type: B, remotePath: string): Promise<GetAclResponse> {
+    const info = await this.storage.bucket(this.bucketName(type)).file(remotePath).acl.get();
+    return info;
+  }
+
+  async exists(type: B, remotePath: string): Promise<boolean> {
+    const [exists] = await this.storage.bucket(this.bucketName(type)).file(remotePath).exists();
+    return exists;
+  }
+
+  async copy(sourceType: B, source: string, destinationType: B, destination: string): Promise<void> {
+    const target = this.storage.bucket(this.bucketName(destinationType)).file(destination);
+    await this.storage.bucket(this.bucketName(sourceType)).file(source).copy(target);
   }
 }
